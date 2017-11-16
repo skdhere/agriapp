a8b690f070a7b56d9f5cd02ed3f2be0d5bc93304
Ejaz Ansari <ejaz@edmission.in>
2017-11-16 18:32:58 +0530

    Personal residence

--- src/pages/personal-details/personal-details.ts
+++ src/pages/personal-details/personal-details.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 /**
  * Generated class for the PersonalDetailsPage page.
  *
@@ -8,18 +8,64 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-personal-details',
-  templateUrl: 'personal-details.html',
-})
-export class PersonalDetailsPage {
+ @IonicPage()
+ @Component({
+ 	selector: 'page-personal-details',
+ 	templateUrl: 'personal-details.html',
+ })
+ export class PersonalDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+ 	personal: FormGroup;
+    submitAttempt: boolean = false;
+    retryButton: boolean = false;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PersonalDetailsPage');
-  }
+ 	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+ 		this.personal = formBuilder.group({
 
-}
+            f1_mfname: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9 ]*'), Validators.required])],
+            f1_mmname: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9 ]*'), Validators.required])],
+            f1_dob: ['', Validators.required],
+            f1_age: [''],
+            f1_altno: ['', Validators.pattern('^[0-9]{10}$')],
+            any_other_select: ['', Validators.required],
+            f1_ppno: ['', Validators.pattern('^[0-9A-Za-z-/\_$&@]{12}$')],
+            f1_pancard: ['', Validators.pattern('^[0-9A-Za-z-/\_$&@]{10}$')],
+            f1_vote: ['', Validators.pattern('^[0-9A-Za-z-/\_$&@]{12}$')],
+            f1_licno: ['', Validators.pattern('^[0-9A-Za-z-/\_$&@]{12}$')],
+            f1_otherno: ['', Validators.compose([Validators.maxLength(20),Validators.pattern('^[0-9A-Za-z-/\_$&@]*')] )],
+            f1_expfarm: ['', Validators.compose([Validators.maxLength(2), Validators.pattern('^[0-9.]*'), Validators.required])],
+        });
+
+        this.personal.controls.f1_dob.valueChanges.subscribe(() => {
+        	let dob = this.personal.controls.f1_dob.value;
+        	this.personal.controls.f1_age.setValue(this.getAge(dob));
+        });
+ 	}
+
+ 	getAge(dateString) {
+	    let today = new Date();
+	    let birthDate = new Date(dateString);
+	    let age = today.getFullYear() - birthDate.getFullYear();
+	    let m = today.getMonth() - birthDate.getMonth();
+	    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+	        age--;
+	    }
+	    return age;
+	}
+
+ 	ionViewDidLoad() {
+        console.log('ionViewDidLoad PersonalDetailsPage');
+    }
+
+    save() {
+
+        this.submitAttempt = true;
+        if (!this.personal.valid) {
+            console.log("Validation error!")
+        } else {
+            console.log("success!")
+            console.log(this.personal.value);
+        }
+    }
+
+ }
--- src/pages/residence-details/residence-details.ts
+++ src/pages/residence-details/residence-details.ts
@@ -1,25 +1,72 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
- * Generated class for the ResidenceDetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ResidenceDetailsPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
-  selector: 'page-residence-details',
-  templateUrl: 'residence-details.html',
+	selector: 'page-residence-details',
+	templateUrl: 'residence-details.html',
 })
 export class ResidenceDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	personal: FormGroup;
+    submitAttempt: boolean = false;
+    retryButton: boolean = false;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResidenceDetailsPage');
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+		this.personal = formBuilder.group({
+            
+            f7_resistatus: ['', Validators.required],
+            f7_phouse: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*')])],
+            f7_pstreet: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*')])],
+            f7_parea: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*'), Validators.required])],
+            f7_pstate: ['', Validators.required],
+            f7_pdistrict: ['', Validators.required],
+            f7_ptaluka: ['', Validators.required],
+            f7_pvillage: ['', Validators.required],
+            f7_ppin: ['', Validators.compose([Validators.pattern('^[0-9]{6}$'), Validators.required])],
 
+            f7_chouse: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*')])],
+            f7_cstreet: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*')])],
+            f7_carea: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z0-9/,\-\@&$# ]*'), Validators.required])],
+            f7_cstate: ['', Validators.required],
+            f7_cdistrict: ['', Validators.required],
+            f7_ctaluka: ['', Validators.required],
+            f7_cvillage: ['', Validators.required],
+            f7_cpin: ['', Validators.compose([Validators.pattern('^[0-9]{6}$'), Validators.required])]
+        });
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad ResidenceDetailsPage');
+	}
+
+	save() {
+
+        this.submitAttempt = true;
+        if (!this.personal.valid) {
+            console.log("Validation error!")
+        } else {
+            console.log("success!")
+            console.log(this.personal.value);
+        }
+    }
+
+    copyAddress() {
+        this.personal.controls['f7_chouse'].setValue(this.personal.value.f7_phouse);
+        this.personal.controls['f7_cstreet'].setValue(this.personal.value.f7_pstreet);
+        this.personal.controls['f7_carea'].setValue(this.personal.value.f7_parea);
+        this.personal.controls['f7_cstate'].setValue(this.personal.value.f7_pstate);
+        this.personal.controls['f7_cdistrict'].setValue(this.personal.value.f7_pdistrict);
+        this.personal.controls['f7_ctaluka'].setValue(this.personal.value.f7_ptaluka);
+        this.personal.controls['f7_cvillage'].setValue(this.personal.value.f7_pvillage);
+        this.personal.controls['f7_cpin'].setValue(this.personal.value.f7_ppin);
+    }
 }
